e6872fa6925b71cebfaa62d962698ce21623f65d
BINTANG XD <94116284+usernameeror@users.noreply.github.com>
2021-12-08 09:31:43 +0700

    Update Sanz.py

--- Sanz.py
+++ Sanz.py
@@ -146,10 +146,10 @@ def moch_yayan():
     os.system('clear')
     print logo
     IP = requests.get('https://www.yayanxd.my.id/server/ip/').text
-    print '___________________________________________________________\n';time.sleep(0.03)
+    print '\033[0;96m___________________________________________________________\n';time.sleep(0.03)
     print ' (\033[0;96m•\033[0m) ACTIVE USER : %s'%(nama);time.sleep(0.03)
     print ' (\033[0;96m•\033[0m) IP DEVICE   : %s'%(IP)
-    print '(\033[0;96m___________________________________________________________\n';time.sleep(0.03)
+    print '\033[0;96m___________________________________________________________\n';time.sleep(0.03)
     print ' [%s1%s]. Dump id dari teman'%(O,N);time.sleep(0.03)
     print ' [%s2%s]. Dump id dari teman publik'%(O,N);time.sleep(0.03)
     print ' [%s3%s]. Dump id dari total followers'%(O,N);time.sleep(0.03)
